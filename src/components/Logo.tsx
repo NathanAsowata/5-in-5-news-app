a26480514b8c0fe962c1a15fed9f2def78f68700
Nathan Asowata <nathanasowata@gmail.com>
2023-08-21 18:07:23 +0100

    chore: setup app skeleton

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Logo.module.scss"
+
+const Logo = () => {
+  return (
+    <span className={styles.logo}>5-in-5</span>
+  )
+}
+
+export default Logo
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Logo from "../components/Logo"
+
+const Home = () => {
+  return (
+    <>
+    <Logo />
+    <h2>Get the top 5 news in 5 categories from 5 cities</h2>
+    <h1>Pick a city</h1>
+  </>
+  )
+}
+
+export default Home
